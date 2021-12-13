2c6411cd311112a01bccc8d79bb128a3f9415bfc
start.vaadin.com <no-reply@vaadin.com>
2021-12-13 16:52:33 +0000

    Generated project

---
+++ src/main/java/com/nfw/application/views/MainLayout.java
@@ -0,0 +1,126 @@
+package com.nfw.application.views;
+
+import com.nfw.application.views.about.AboutView;
+import com.nfw.application.views.favorite.FavoriteView;
+import com.nfw.application.views.login.LoginView;
+import com.nfw.application.views.search.SearchView;
+import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Header;
+import com.vaadin.flow.component.html.ListItem;
+import com.vaadin.flow.component.html.Nav;
+import com.vaadin.flow.component.html.Span;
+import com.vaadin.flow.component.html.UnorderedList;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * The main view is a top-level placeholder for other views.
+ */
+@PWA(name = "News From World", shortName = "News From World", enableInstallPrompt = false)
+@Theme(themeFolder = "newsfromworld")
+@PageTitle("Main")
+public class MainLayout extends AppLayout {
+
+    public static class MenuItemInfo {
+
+        private String text;
+        private String iconClass;
+        private Class<? extends Component> view;
+
+        public MenuItemInfo(String text, String iconClass, Class<? extends Component> view) {
+            this.text = text;
+            this.iconClass = iconClass;
+            this.view = view;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public String getIconClass() {
+            return iconClass;
+        }
+
+        public Class<? extends Component> getView() {
+            return view;
+        }
+
+    }
+
+    public MainLayout() {
+        addToNavbar(createHeaderContent());
+    }
+
+    private Component createHeaderContent() {
+        Header header = new Header();
+        header.addClassNames("bg-base", "border-b", "border-contrast-10", "box-border", "flex", "flex-col", "w-full");
+
+        Div layout = new Div();
+        layout.addClassNames("flex", "h-xl", "items-center", "px-l");
+
+        H1 appName = new H1("News From World");
+        appName.addClassNames("my-0", "me-auto", "text-l");
+        layout.add(appName);
+
+        Nav nav = new Nav();
+        nav.addClassNames("flex", "gap-s", "overflow-auto", "px-m");
+
+        // Wrap the links in a list; improves accessibility
+        UnorderedList list = new UnorderedList();
+        list.addClassNames("flex", "list-none", "m-0", "p-0");
+        nav.add(list);
+
+        for (RouterLink link : createLinks()) {
+            ListItem item = new ListItem(link);
+            list.add(item);
+        }
+
+        header.add(layout, nav);
+        return header;
+    }
+
+    private List<RouterLink> createLinks() {
+        MenuItemInfo[] menuItems = new MenuItemInfo[]{ //
+                new MenuItemInfo("Login", "la la-globe", LoginView.class), //
+
+                new MenuItemInfo("About", "la la-file", AboutView.class), //
+
+                new MenuItemInfo("Search", "la la-file", SearchView.class), //
+
+                new MenuItemInfo("Favorite", "la la-file", FavoriteView.class), //
+
+        };
+        List<RouterLink> links = new ArrayList<>();
+        for (MenuItemInfo menuItemInfo : menuItems) {
+            links.add(createLink(menuItemInfo));
+
+        }
+        return links;
+    }
+
+    private static RouterLink createLink(MenuItemInfo menuItemInfo) {
+        RouterLink link = new RouterLink();
+        link.addClassNames("flex", "h-m", "items-center", "px-s", "relative", "text-secondary");
+        link.setRoute(menuItemInfo.getView());
+
+        Span icon = new Span();
+        icon.addClassNames("me-s", "text-l");
+        if (!menuItemInfo.getIconClass().isEmpty()) {
+            icon.addClassNames(menuItemInfo.getIconClass());
+        }
+
+        Span text = new Span(menuItemInfo.getText());
+        text.addClassNames("font-medium", "text-s", "whitespace-nowrap");
+
+        link.add(icon, text);
+        return link;
+    }
+
+}
---
+++ src/main/java/com/nfw/application/views/login/LoginView.java
@@ -0,0 +1,33 @@
+package com.nfw.application.views.login;
+
+import com.nfw.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Login")
+@Route(value = "login", layout = MainLayout.class)
+@RouteAlias(value = "", layout = MainLayout.class)
+public class LoginView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public LoginView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
